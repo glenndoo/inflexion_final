b3ab478e504fcfebc2219bf23956072242e7a608
glenndoo <60509624+glenndoo@users.noreply.github.com>
2021-04-24 14:59:54 +0800

    Register try

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+                {!! Form::open() !!}
+                    {{ Form::email('email')}}
+                    {{ Form::password('password')}}
+                    {{ Form::select('type', array(
+                        'Student','Tutor'))}}
+                    {{ Form::submit('Register')}}
+                {!! Form::close() !!}                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
